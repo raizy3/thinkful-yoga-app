0b9cbed138e3ebf8a954299aa8afe96543c3d00d
Raizy Einhorn <raizyeinhorn@gmail.com>
2020-03-04 17:36:32 -0500

    rendered quiz form in js

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 const STORE=[
     {
 
-        photo:"easy-pose.jpg",
+        photo:"yoga-images/easy-pose.jpg",
         options:["Easy Pose","Meditation Pose","Crosslegged Pose","Prayer Pose"],
         correctAnswer:"Easy Pose"},
     
     {
 
-        photo:"downward-dog.jpg",
+        photo:"yoga-images/downward-dog.jpg",
         options:["Downward Dog", "Gosh, I just noticed that I need a manicure",
             "Triangle Pose", "Calf Stretch"],
         correctAnswer:"Downward Dog"
     },
 
     {
-        photo:"side-plank-pose.jpg",
+        photo:"yoga-images/side-plank-pose.jpg",
         options:["This is way harder than it looks", "Side Plank", "Extended Side Twist",
                     "Reach for the Stars Pose"],
         correctAnswer:"Side Plank"
     },
 
     {
-        photo:"extended-childs-pose.jpg",
+        photo:"yoga-images/extended-childs-pose.jpg",
         options:["Puppy Posture", "Worship Pose", "Extended Child's Pose",
                     "Finally, a break!"],
         correctAnswer:"Extended Child's Pose"
@@ -34,35 +34,87 @@ const STORE=[
         correctAnswer:"Dancer Pose"
     }
 ]
+//set values for current question and 
+let currentQuestion=0;
+let score=0;
+function startQuiz(){
+   // $("#start").on('click', function (event){
+        renderQuiz();
+  //  });
+}
+//Display options for each question
+function generateOptionsString(){
+    console.log('generating question');
 
-//Start the quiz
+    let myOptions = STORE[currentQuestion].options;
+
+    for(let i=0;i<myOptions.length;i++){
+        $(".js-options").append(`<input type="radio" name="pose" value="${myOptions[i]}" id="pose-${i+1}" class="multiple-choice"required> <label for="pose-${i+1}">${myOptions[i]}</label><br>`);
+    };
+ }
+//creates the HTML for the question pages
 function renderQuiz(){
-    console.log('`renderQuiz` ran')
-    const quizStartString='<form><fieldset><legend>What is the Name of this Yoga Pose?</legend> <div> <img src="yoga-images/easy-pose.jpg" alt="woman sitting in a crosslegged seat"></div></fieldset></form>';
+    console.log('`renderQuiz` ran');
+    let photo = STORE[currentQuestion].photo;
+
+    const quizStartString=`<form class=><fieldset><legend>What is the Name of this Yoga Pose?<img class="photo" src="${photo}" alt="Woman in a crosslegged seat with hands at her heart"></legend><div class="js-options"></div><button type="submit" class="js-submit" >Submit</button><button type="submit" class="next">Next</button></fieldset></form>`;
 
     $('.js-question-box').html(quizStartString);
+
+    generateOptionsString();
+
+    whatQuestion();
+
+    displayScore();
+
+    $('.next').hide();
     //form should be rendered in ".js-question-box"
     //form should include a legend with question
     //loop through objects in STORE and render 'photo' in the div
     //and render 'options' as radio inputs
     //render a button to check answer
+
+
 }
 
 //display the question number
 function whatQuestion(){
     console.log('`whatQuestion ran')
+    $(".js-header-display").html(`<h2>Question: ${currentQuestion+1}</h2>`)
     //Display the number of the question the header
 }
 
 //responsible to display number of correct answers so far
 function displayScore(){
+    console.log('displaying Score');
+    $(".js-header-display").append(`<h2>Score: ${score}/5</h2>`);
 
 }
 
 //responsible to give feedback on whether answer is correct or incorrect
 function rightAnswer(){
-
+    $('.js-submit').hide;
+    $('.next').show;
+    let answer = $("input[name=pose]:checked").val();
+    let correct = STORE[currentQuestion].correctAnswer;
+    if(answer === correct){
+        score++;
+        $('js-options').append('<p>Correct!</p>');
+    }
+    else{
+        $('js-options').append(`<p>Nope, the correct answer is ${correct}`)
+    };
 }
+
+//Answer feedback on submit
+function getAnswerFeedback(){
+    $('form').on('click', '.js-submit', function(e){
+        console.log('is your answer correct?')
+        e.preventDefault();
+        rightAnswer();
+    });
+}
+
 
 
 //responsible to take user to next question
@@ -77,7 +129,8 @@ function yourResults(){
 
 //Calls the above functions
 function runQuiz(){
-    renderQuiz();
+    startQuiz();
+    getAnswerFeedback();
     nextQuestion();
     yourResults();
 }
