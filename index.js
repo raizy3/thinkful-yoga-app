c377f1119df2bbdff8c1b30db016563468d0d27a
Raizy Einhorn <raizyeinhorn@gmail.com>
2020-03-05 13:21:15 -0500

    fixed required and updated the object to include alts

--- index.js
+++ index.js
@@ -3,35 +3,44 @@ const STORE=[
 
         photo:"yoga-images/easy-pose.jpg",
         options:["Easy Pose","Meditation Pose","Crosslegged Pose","Prayer Pose"],
-        correctAnswer:"Easy Pose"},
+        correctAnswer:"Easy Pose",
+        alt:"woman in a crosslegged seat with hands at her heart"
+    },
+        
+    
     
     {
 
         photo:"yoga-images/downward-dog.jpg",
         options:["Downward Dog", "Gosh, I just noticed that I need a manicure",
             "Triangle Pose", "Calf Stretch"],
-        correctAnswer:"Downward Dog"
+        correctAnswer:"Downward Dog",
+        alt: "woman with hands and legs on the ground and hips in the air",
     },
 
     {
         photo:"yoga-images/side-plank-pose.jpg",
         options:["This is way harder than it looks", "Side Plank", "Extended Side Twist",
                     "Reach for the Stars Pose"],
-        correctAnswer:"Side Plank"
+        correctAnswer:"Side Plank",
+        alt:"woman on her side lifting knees and hips off the floor"
     },
 
     {
         photo:"yoga-images/extended-childs-pose.jpg",
         options:["Puppy Posture", "Worship Pose", "Extended Child's Pose",
                     "Finally, a break!"],
-        correctAnswer:"Extended Child's Pose"
+        correctAnswer:"Extended Child's Pose",
+        alt:"woman kneeling on the ground with torso lying between knees and arms extended if front of her"
     },
 
     {
         photo:"yoga-images/dancer-pose.jpg",
         options:["Nope, not even gonna try that one", "Dancer Pose",
                     "One-legged Tree Pose", "Balance Posture"],
-        correctAnswer:"Dancer Pose"
+        correctAnswer:"Dancer Pose",
+        alt:"woman balancing on 1 leg, holding 1 knee and extending her torso",
+
     }
 ]
 //set values for current question and 
@@ -54,7 +63,7 @@ function generateOptionsString(){
     let myOptions = STORE[currentQuestion].options;
 
     for(let i=0;i<myOptions.length;i++){
-        $(".js-options").append(`<input type="radio" name="pose" value="${myOptions[i]}" id="pose-${i+1}" class="multiple-choice"required> <label for="pose-${i+1}">${myOptions[i]}</label><br>`);
+        $(".js-options").append(`<input type="radio" name="pose" value="${myOptions[i]}" id="pose-${i+1}" class="multiple-choice"> <label for="pose-${i+1}">${myOptions[i]}</label><br>`);
     };
  }
 
@@ -62,8 +71,8 @@ function generateOptionsString(){
 function renderQuiz(currentQues){
     console.log('`renderQuiz` ran');
     let photo = STORE[currentQues].photo;
-
-    const quizStartString=`<form class=><fieldset><legend>What is the Name of this Yoga Pose?<img class="photo" src="${photo}" alt="Woman in a crosslegged seat with hands at her heart"></legend><div class="js-options"></div><button type="submit" class="js-submit" >Submit</button><button type="submit" class="next">Next</button></fieldset></form>`;
+    let alt =   STORE[currentQues].alt;
+    const quizStartString=`<form class=><fieldset><legend>What is the Name of this Yoga Pose?<img class="photo" src="${photo}" alt="${alt}"></legend><div class="js-options"></div><button type="submit" class="js-submit" >Submit</button><button type="submit" class="next">Next</button></fieldset></form>`;
 
     $('.js-question-box').html(quizStartString);
 
@@ -94,14 +103,20 @@ function displayScore(){
 
 //responsible to give feedback on whether answer is correct or incorrect
 function rightAnswer(){
+
+    let answer = $("input[name=pose]:checked").val();
+    let correct = STORE[currentQuestion].correctAnswer;
+
     console.log(`what's your answer?`)
+    if(!answer){
+        alert('Please choose an option');
+     return;};
     $('.js-submit').hide();
     $('.multiple-choice').attr('disabled', true );
     $('.next').show();
 
-   let answer = $("input[name=pose]:checked").val();
-    let correct = STORE[currentQuestion].correctAnswer;
-    
+   
+   
     if(answer === correct){
         score++;
 
@@ -112,7 +127,6 @@ function rightAnswer(){
         $('.js-options').append(`<p>Nope, the correct answer is ${correct}`)
     };
 
-    currentQuestion++;
 
     
    
@@ -138,6 +152,7 @@ function nextQuestion(){
         }
        
         else{
+        currentQuestion++
         renderQuiz(currentQuestion);
         getAnswerFeedback();
     };
